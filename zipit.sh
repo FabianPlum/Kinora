cb24f19467244698afbc30aad71fd4539e443150
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2026-03-26 11:49:06 +0100

    Add examples to zip

--- zipit.sh
+++ zipit.sh
@@ -4,11 +4,10 @@ set -euo pipefail
 zip -r BlenderJPS.zip blender_jps \
     -x "blender_jps/deps/*" \
     -x "blender_jps/tests/*" \
-    -x "blender_jps/examples/*" \
+    -x "blender_jps/examples/040_l020_g1_rf_h-.h5" \
     -x "blender_jps/__pycache__/*" \
     -x "blender_jps/**/__pycache__/*" \
     -x "*.pyc" \
     -x "*~" \
     -x "*.~undo-tree~" \
-    -x "*.sqlite" \
     -x "*.DS_Store"
